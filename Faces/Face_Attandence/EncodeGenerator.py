c2ae966fe6ce5fec8e04349acd0c810102e4db3c
Araz Moazzemi <67405217+arazmoazzemi@users.noreply.github.com>
2024-01-02 09:33:00 +0330

    Update EncodeGenerator.py

--- Faces/Face_Attandence/EncodeGenerator.py
+++ Faces/Face_Attandence/EncodeGenerator.py
@@ -1,3 +1,42 @@
-import cv2
+import cv2, os
 import face_recognition
+import pickle
+
+# import students Images
+folderPath = 'Images'
+PathList = os.listdir(folderPath)
+print(PathList)
+imgList = []
+studentIds = []
+for path in PathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    # print(path)
+    # print(os.path.splitext(path)[0])
+    studentIds.append(os.path.splitext(path)[0])
+print(studentIds)
+
+
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+print("Encoding Started ...")
+encodeListKnown = findEncodings(imgList)
+encodelistKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
+
+file = open(("EncodeFile.p"), 'wb')
+pickle.dump(encodelistKnownWithIds, file)
+file.close()
+
+
+
+
+
 
